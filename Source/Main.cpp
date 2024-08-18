e27a950771647aad6dbf4a6ab0ef234c85734592
shivaduke28 <shivaduke28@gmail.com>
2024-08-18 21:40:09 +0900

    add namespace juce_dj

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,66 +1,69 @@
 #include <JuceHeader.h>
 #include "MainComponent.h"
 
-class JuceDJApplication : public juce::JUCEApplication
+namespace juce_dj
 {
-public:
-    JuceDJApplication() {}
-
-    const juce::String getApplicationName() override { return ProjectInfo::projectName; }
-    const juce::String getApplicationVersion() override { return ProjectInfo::versionString; }
-    bool moreThanOneInstanceAllowed() override { return true; }
-
-    void initialise(const juce::String& commandLine) override
-    {
-        mainWindow.reset(new MainWindow(getApplicationName()));
-    }
-
-    void shutdown() override
-    {
-        mainWindow = nullptr;
-    }
-
-    void systemRequestedQuit() override
-    {
-        quit();
-    }
-
-    void anotherInstanceStarted(const juce::String& commandLine) override
-    {
-    }
-    class MainWindow : public juce::DocumentWindow
+    class JuceDJApplication : public juce::JUCEApplication
     {
     public:
-        MainWindow(juce::String name)
-            : DocumentWindow(name,
-                juce::Desktop::getInstance().getDefaultLookAndFeel()
-                .findColour(juce::ResizableWindow::backgroundColourId),
-                DocumentWindow::allButtons)
+        JuceDJApplication() {}
+
+        const juce::String getApplicationName() override { return ProjectInfo::projectName; }
+        const juce::String getApplicationVersion() override { return ProjectInfo::versionString; }
+        bool moreThanOneInstanceAllowed() override { return true; }
+
+        void initialise(const juce::String& commandLine) override
         {
-            setUsingNativeTitleBar(true);
-            setContentOwned(new MainComponent(), true);
+            mainWindow.reset(new MainWindow(getApplicationName()));
+        }
+
+        void shutdown() override
+        {
+            mainWindow = nullptr;
+        }
+
+        void systemRequestedQuit() override
+        {
+            quit();
+        }
+
+        void anotherInstanceStarted(const juce::String& commandLine) override
+        {
+        }
+        class MainWindow : public juce::DocumentWindow
+        {
+        public:
+            MainWindow(juce::String name)
+                : DocumentWindow(name,
+                    juce::Desktop::getInstance().getDefaultLookAndFeel()
+                    .findColour(juce::ResizableWindow::backgroundColourId),
+                    DocumentWindow::allButtons)
+            {
+                setUsingNativeTitleBar(true);
+                setContentOwned(new MainComponent(), true);
 
 #if JUCE_IOS || JUCE_ANDROID
-            setFullScreen(true);
+                setFullScreen(true);
 #else
-            setResizable(true, true);
-            centreWithSize(getWidth(), getHeight());
+                setResizable(true, true);
+                centreWithSize(getWidth(), getHeight());
 #endif
 
-            setVisible(true);
-        }
+                setVisible(true);
+            }
 
-        void closeButtonPressed() override
-        {
-            JUCEApplication::getInstance()->systemRequestedQuit();
-        }
+            void closeButtonPressed() override
+            {
+                JUCEApplication::getInstance()->systemRequestedQuit();
+            }
+
+        private:
+            JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainWindow)
+        };
 
     private:
-        JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainWindow)
+        std::unique_ptr<MainWindow> mainWindow;
     };
 
-private:
-    std::unique_ptr<MainWindow> mainWindow;
-};
-
-START_JUCE_APPLICATION(JuceDJApplication)
+    START_JUCE_APPLICATION(JuceDJApplication)
+}
--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -2,53 +2,56 @@
 
 #include <JuceHeader.h>
 
-class MainComponent : public juce::AudioAppComponent
+namespace juce_dj
 {
-public:
-    MainComponent()
+    class MainComponent : public juce::AudioAppComponent
     {
-        setSize(800, 600);
-
-        if (juce::RuntimePermissions::isRequired(juce::RuntimePermissions::recordAudio)
-            && !juce::RuntimePermissions::isGranted(juce::RuntimePermissions::recordAudio))
+    public:
+        MainComponent()
         {
-            juce::RuntimePermissions::request(juce::RuntimePermissions::recordAudio,
-                [&](bool granted) { setAudioChannels(granted ? 2 : 0, 2); });
+            setSize(800, 600);
+
+            if (juce::RuntimePermissions::isRequired(juce::RuntimePermissions::recordAudio)
+                && !juce::RuntimePermissions::isGranted(juce::RuntimePermissions::recordAudio))
+            {
+                juce::RuntimePermissions::request(juce::RuntimePermissions::recordAudio,
+                    [&](bool granted) { setAudioChannels(granted ? 2 : 0, 2); });
+            }
+            else
+            {
+                setAudioChannels(2, 2);
+            }
         }
-        else
+
+        ~MainComponent() override
         {
-            setAudioChannels(2, 2);
+            shutdownAudio();
         }
-    }
 
-    ~MainComponent() override
-    {
-        shutdownAudio();
-    }
+        void prepareToPlay(int samplesPerBlockExpected, double sampleRate) override
+        {
+        }
 
-    void prepareToPlay(int samplesPerBlockExpected, double sampleRate) override
-    {
-    }
+        void getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill) override
+        {
+            bufferToFill.clearActiveBufferRegion();
+        }
 
-    void getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill) override
-    {
-        bufferToFill.clearActiveBufferRegion();
-    }
+        void releaseResources() override
+        {
+        }
 
-    void releaseResources() override
-    {
-    }
+        void paint(juce::Graphics& g) override
+        {
+            g.fillAll(getLookAndFeel().findColour(juce::ResizableWindow::backgroundColourId));
+        }
 
-    void paint(juce::Graphics& g) override
-    {
-        g.fillAll(getLookAndFeel().findColour(juce::ResizableWindow::backgroundColourId));
-    }
-
-    void resized() override
-    {
-    }
+        void resized() override
+        {
+        }
 
 
-private:
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainComponent)
-};
+    private:
+        JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainComponent)
+    };
+}
